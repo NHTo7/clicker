70eb04b32ecdf10658c5b4dd26acedb60b1c26ba
NHTo7 <145566131+NHTo7@users.noreply.github.com>
2024-03-07 20:34:18 +0100

    done 1.1

--- CS/script.js
+++ CS/script.js
@@ -1,7 +1,18 @@
-let clickPoints = 0 + localStorage.getItem("cookie_clickPoints");
-let counter = + localStorage.getItem("cookie_clicks");
-let upgrade_1 = + localStorage.getItem("cookie_upgrade_1");
-let upgrade_2 = + localStorage.getItem("cookie_upgrade_2");
+if (localStorage.getItem("cookie_clicks")) {
+    var clickPoints = + localStorage.getItem("cookie_clickPoints");
+    var counter = + localStorage.getItem("cookie_clicks");
+    var upgrade_1 = + localStorage.getItem("cookie_upgrade_1");
+    var upgrade_2 = + localStorage.getItem("cookie_upgrade_2");
+    var autoClickerInterval = + localStorage.getItem("cookie_autoClickerInterval")
+}
+
+else {
+    var clickPoints = 1
+    var counter = 0
+    var upgrade_1 = 10
+    var upgrade_2 = 100
+    var autoClickerInterval = 0
+}
 
 document.getElementById('button').onclick = () => {
     counter += clickPoints;
@@ -17,10 +28,21 @@ document.getElementById('upgrade_1').onclick = () => {
     }
 
 };
+
+document.getElementById('upgrade_2').onclick = () => {
+    if (counter >= upgrade_2) {
+        counter -= upgrade_2;
+        upgrade_2 *= 1.5; // Increase the cost of upgrade 2 each time it's purchased
+        autoClickerInterval += 1
+        update();
+    }
+};
+
 function update() {
-    document.getElementById('upgrade_1').innerText = '+ 1 per click - ' + Math.round(upgrade_1);
-    document.getElementById('score').innerText = Math.round(counter);
-    save_data()
+    document.getElementById('upgrade_1').innerText = '+ $1 per click - Cost: $' + Math.round(upgrade_1);
+    document.getElementById('upgrade_2').innerText = 'Upgrade Level 2 - Cost: $' + Math.round(upgrade_2);
+    document.getElementById('score').innerText = '$' + Math.round(counter);
+    save_data();
 }
 
 function save_data() {
@@ -28,6 +50,19 @@ function save_data() {
     localStorage.setItem('cookie_clicks', counter)
     localStorage.setItem('cookie_upgrade_1', upgrade_1)
     localStorage.setItem('cookie_upgrade_2', upgrade_2)
+    localStorage.setItem('cookie_autoClickerInterval', autoClickerInterval)
 }
+
+function tick() {
+    counter += autoClickerInterval
+    update()
+}
+
+document.getElementById("restartButton").onclick = () => {
+    localStorage.clear();
+    location.reload();
+}
+
+setInterval(tick, 1000);
 
 update()
